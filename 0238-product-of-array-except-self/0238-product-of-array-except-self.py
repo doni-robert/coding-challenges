7b8b0d9fea7c957facdd3b4e3db4e18cb090586d
Robert Ndung'u <105845754+doni-robert@users.noreply.github.com>
2024-06-17 07:20:51 +0300

    Time: 289 ms (47.57%), Space: 21.7 MB (27.22%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -4,32 +4,23 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        # Determine the length of the input list
-        length = len(nums)
+        n = len(nums)
         
-        # Initialize the answer list with 1s to store the final results.
-        ans = length * [1]
+        # Initialize arrays for left and right products
+        left_products = [1] * n
+        right_products = [1] * n
+        answer = [1] * n
         
-        # Populate the answer list with products of elements to the right of each index
-        # Start from the second last element and move backwards to the first element
-        for i in range(length-2, -1, -1):
-            # Each element at index i is updated to be the product of the element
-            # to its right and the product stored at the index right of i
-            ans[i] = ans[i+1] * nums[i+1]
+        # Fill left_products
+        for i in range(1, n):
+            left_products[i] = left_products[i - 1] * nums[i - 1]
         
-        # Initialize a variable to keep track of the cumulative product of elements
-        # to the left of the current index
-        prefexProduct = 1
+        # Fill right_products
+        for i in range(n - 2, -1, -1):
+            right_products[i] = right_products[i + 1] * nums[i + 1]
         
-        # Update the answer list by multiplying each element by the cumulative product
-        # of all elements to the left of the current index
-        for i in range(0, length):
-            # Multiply the current element in ans by the cumulative left product
-            ans[i] *= prefexProduct
-            
-            # Update the cumulative left product by multiplying it with the current element in nums
-            prefexProduct *= nums[i]
+        # Fill the answer array
+        for i in range(n):
+            answer[i] = left_products[i] * right_products[i]
         
-        # Return the final answer list, which contains the product of all elements
-        # except the one at each index
-        return ans
+        return answer
