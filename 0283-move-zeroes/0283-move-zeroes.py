9d704b51d7da5d66b516ee5e40043bcbd976ecc8
Robert Ndung'u <105845754+doni-robert@users.noreply.github.com>
2024-06-19 07:01:43 +0300

    Time: 108 ms (90.09%), Space: 13.2 MB (9.26%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -1,14 +1,26 @@
-class Solution(object):
-    def moveZeroes(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: None Do not return anything, modify nums in-place instead.
-        """
-        length = len(nums)
+# class Solution(object):
+#     def moveZeroes(self, nums):
+#         """
+#         :type nums: List[int]
+#         :rtype: None Do not return anything, modify nums in-place instead.
+#         """
+#         length = len(nums)
 
-        for i in range(length):
-            if nums[i] == 0:
-                nums.append(0)
-                nums[i] = None
-        while None in nums:
-            nums.remove(None)
+#         for i in range(length):
+#             if nums[i] == 0:
+#                 nums.append(0)
+#                 nums[i] = None
+#         while None in nums:
+#             nums.remove(None)
+
+# 2 Pointer solution
+class Solution:
+    def moveZeroes(self, nums):
+        left = 0
+
+        for right in range(len(nums)):
+            if nums[right] != 0:
+                nums[right], nums[left] = nums[left], nums[right]
+                left += 1
+        
+        return nums
